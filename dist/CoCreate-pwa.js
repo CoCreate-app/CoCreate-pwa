d1e856eaef52dda21c9aa83402d20ddf79b6f1f3
ReactDev313 <reactist313@hotmail.com>
2021-03-14 20:40:50 -0600

    fix issues pwa

---
+++ dist/CoCreate-pwa.js
@@ -0,0 +1,45 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory();
+	else if(typeof define === 'function' && define.amd)
+		define([], factory);
+	else if(typeof exports === 'object')
+		exports["CoCreate"] = factory();
+	else
+		root["CoCreate"] = root["CoCreate"] || {}, root["CoCreate"]["pwa"] = factory();
+})(this, function() {
+return /******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/CoCreate-pwa.js":
+/*!*****************************!*\
+  !*** ./src/CoCreate-pwa.js ***!
+  \*****************************/
+/***/ (() => {
+
+eval("// component implementation:\n// 1. it should have programming api as well as html or attribute api\n// 2. Includes semantic//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9Db0NyZWF0ZS5wd2EvLi9zcmMvQ29DcmVhdGUtcHdhLmpzPzcxYmEiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUE7QUFDQTtBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiLy8gY29tcG9uZW50IGltcGxlbWVudGF0aW9uOlxyXG4vLyAxLiBpdCBzaG91bGQgaGF2ZSBwcm9ncmFtbWluZyBhcGkgYXMgd2VsbCBhcyBodG1sIG9yIGF0dHJpYnV0ZSBhcGlcclxuLy8gMi4gSW5jbHVkZXMgc2VtYW50aWNcclxuIl0sImZpbGUiOiIuL3NyYy9Db0NyZWF0ZS1wd2EuanMuanMiLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///./src/CoCreate-pwa.js\n");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval-source-map devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/CoCreate-pwa.js"]();
+/******/ 	__webpack_exports__ = __webpack_exports__.default;
+/******/ 	
+/******/ 	return __webpack_exports__;
+/******/ })()
+;
+});
