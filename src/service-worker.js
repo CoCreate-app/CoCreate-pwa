6501b9b148554e0a88d97220fb253f35e2d0785f
ReactDev313 <reactist313@hotmail.com>
2021-03-15 16:04:19 -0600

    update service-worker

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,52 +1,70 @@
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'offline.html';
+"use strict";
 
-self.addEventListener('install', function(event) {
-  console.log('[ServiceWorker] Install');
-  
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
-    // Setting {cache: 'reload'} in the new request will ensure that the response
-    // isn't fulfilled from the HTTP cache; i.e., it will be from the network.
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-  })());
-  
-  self.skipWaiting();
+// Incrementing CACHE_VERSION will start the install event and
+// force previously cached resources to be cached again.
+const CACHE_VERSION = "{{ site.time }}";
+let CURRENT_CACHES = {
+  offline: "offline-v" + CACHE_VERSION,
+};
+
+const OFFLINE_URL = "offline.html";
+
+function createCacheBustedRequest(url) {
+  let request = new Request(url, { cache: "reload" });
+  // See https://fetch.spec.whatwg.org/#concept-request-mode
+  // This is not yet supported in Chrome as of M48, so we need to explicitly check to see if the cache: 'reload' option had any effect.
+  if ("cache" in request) {
+    return request;
+  }
+
+  // If {cache: 'reload'} didn't have any effect, append a cache-busting URL parameter instead.
+  let bustedUrl = new URL(url, self.location.href);
+  bustedUrl.search += (bustedUrl.search ? "&" : "") + "cachebust=" + Date.now();
+  return new Request(bustedUrl);
+}
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    fetch(createCacheBustedRequest(OFFLINE_URL)).then(function (response) {
+      return caches.open(CURRENT_CACHES.offline).then(function (cache) {
+        return cache.put(OFFLINE_URL, response);
+      });
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('[ServiceWorker] Activate');
-  event.waitUntil((async () => {
-    // Enable navigation preload if it's supported.
-    // See https://developers.google.com/web/updates/2017/02/navigation-preload
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
+self.addEventListener("activate", (event) => {
+  // Delete all caches that aren't named in CURRENT_CACHES.
+  let expectedCacheNames = Object.keys(CURRENT_CACHES).map(function (key) {
+    return CURRENT_CACHES[key];
+  });
 
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (expectedCacheNames.indexOf(cacheName) === -1) {
+            // If this cache name isn't present in the array of "expected" cache names, then delete it.
+            console.log("Deleting out of date cache:", cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', function(event) {
-  // console.log('[Service Worker] Fetch', event.request.url);
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
-
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        console.log('[Service Worker] Fetch failed; returning offline page instead.', error);
-
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    })());
+self.addEventListener("fetch", (event) => {
+  if (
+    event.request.mode === "navigate" ||
+    (event.request.method === "GET" &&
+      event.request.headers.get("accept").includes("text/html"))
+  ) {
+    console.log("Handling fetch event for", event.request.url);
+    event.respondWith(
+      fetch(createCacheBustedRequest(event.request.url)).catch((error) => {
+        return caches.match(OFFLINE_URL);
+      })
+    );
   }
 });
