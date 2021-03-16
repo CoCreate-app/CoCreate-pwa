b6b9f3c12fda47ef7b52bd7ef8ec920a65c1981a
frankpagan <frank@cocreate.app>
2021-03-16 18:57:50 +0000

    Reverting to c093510

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,61 @@
-const path = require("path")
-const TerserPlugin = require("terser-webpack-plugin")
+// Webpack uses this to work with directories
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-let isProduction = process.env.NODE_ENV === "production"
-const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+let isProduction = process.env.NODE_ENV === 'production';
 
+// This is main configuration object.
+// Here you write different options and tell Webpack what to do
 module.exports = {
+
+  // Path to your entry point. From this file Webpack will begin his work
   entry: {
-    "CoCreate-pwa": "./src/CoCreate-pwa.js",
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: isProduction ? "[name].min.js" : "[name].js",
-    libraryTarget: "umd",
-    libraryExport: "default",
-    library: ["CoCreate", "pwa"],
-    globalObject: "this",
-    publicPath: "https://server.cocreate.app/CoCreateJS/dist/",
+    'CoCreate-pwa': './src/CoCreate-pwa.js',
   },
 
-  plugins: [new CleanWebpackPlugin()],
+ // Path and filename of your result bundle.
+  // Webpack will bundle all JavaScript into this file
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: isProduction ? '[name].min.js' : '[name].js',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    library: ['CoCreate', 'pwa'],
+    globalObject: "this",
+  },
+
   // Default mode for Webpack is production.
   // Depending on mode Webpack will apply different things
   // on final bundle. For now we don't need production's JavaScript
   // minifying and other thing so let's set mode to development
-  mode: isProduction ? "production" : "development",
+  mode: isProduction ? 'production' : 'development',
   module: {
-    rules: [
-      {
-        test: /.js$/,
+    rules: [{
+        test: /\.js$/,
         exclude: /(node_modules)/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            plugins: ["@babel/plugin-transform-modules-commonjs"],
-          },
-        },
+            presets: ['@babel/preset-env']
+          }
+        }
       },
-      {
-        test: /.css$/i,
-        use: [
-          { loader: "style-loader", options: { injectType: "linkTag" } },
-          "file-loader",
-        ],
-      },
-    ],
+    ]
   },
 
   // add source map
-  ...(isProduction ? {} : { devtool: "eval-source-map" }),
+  ...(isProduction ? {} : { devtool: 'eval-source-map' }),
 
+  // add uglifyJs
   optimization: {
-    minimize: true,
-    minimizer: [
-      new TerserPlugin({
-        extractComments: true,
-        // cache: true,
-        parallel: true,
-        // sourceMap: true, // Must be set to true if using source-maps in production
-        terserOptions: {
-          // https://github.com/webpack-contrib/terser-webpack-plugin#terseroptions
-          // extractComments: 'all',
-          compress: {
-            drop_console: true,
-          },
-        },
-      }),
-    ],
-    splitChunks: {
-      chunks: "all",
-      minSize: 200,
-      // maxSize: 999999999,
-      //minChunks: 1,
-      cacheGroups: {
-        defaultVendors: false,
+    minimizer: [new UglifyJsPlugin({
+      uglifyOptions: {
+        // get options: https://github.com/mishoo/UglifyJS
+        drop_console: isProduction
       },
-    },
+    })],
   },
-}
+
+};
